86119c53a721c8f8e88c7ed49d9223630c813b41
ojayyezzir <ojayyezzir@gmail.com>
2020-11-17 15:26:20 +0900

    Update setup: change version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pytenet',
-    version='0.0.1',
+    version='0.0.2',
     description='Don’t try to understand it. Feel it.',
     long_description=long_description,
     long_description_content_type='text/markdown',
